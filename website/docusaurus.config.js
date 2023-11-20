c50a5c7b3ec9b845638e4265a82d6f0b39c74738
Jonathan Wilde <jonathan@realworldprivacy.com>
2023-11-19 17:21:57 -0700

    fix

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "realworldprivacy", // Usually your GitHub org/user name.
+  organizationName: "hellojwilde", // Usually your GitHub org/user name.
   projectName: "energetic-ai", // Usually your repo name.
 
   onBrokenLinks: "throw",
